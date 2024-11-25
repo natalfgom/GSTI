6fef8af0c4d6034567844a84e0091d84b52e5142
YasmineLD <yaslakdar@alum.us.es>
2024-11-25 19:06:56 +0100

    Cambio1

---
+++ app/src/main/java/com/example/gsti/menuInicio/InicioPaciente.kt
@@ -0,0 +1,12 @@
+package com.example.gsti.menuInicio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gsti.R
+
+class InicioPaciente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_paciente) // Referencia al layout
+    }
+}
