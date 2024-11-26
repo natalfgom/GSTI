d695ae9be211ce91487b891c9f0f5c410faf1259
YasmineLD <yaslakdar@alum.us.es>
2024-11-26 16:39:14 +0100

    Pantallas de inicio

---
+++ app/src/main/java/com/example/gsti/menuInicio/InicioFamiliar.kt
@@ -0,0 +1,12 @@
+package com.example.gsti.menuInicio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gsti.R
+
+class InicioFamiliar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_familiar) // Referencia al layout
+    }
+}
