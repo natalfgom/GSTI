1b5e7dd061dd314bc731e807df3ea8cf52d12b75
YasmineLD <yaslakdar@alum.us.es>
2024-11-26 17:53:59 +0100

    Sobre nosotros

--- app/src/main/java/com/example/gsti/MainActivity.kt
+++ app/src/main/java/com/example/gsti/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.inicio_paciente)
+        setContentView(R.layout.sobre_nosotros)
     }
 }
---
+++ app/src/main/java/com/example/gsti/SobreNosotros.kt
@@ -0,0 +1,13 @@
+package com.example.gsti
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gsti.R
+
+class SobreNosotros : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sobre_nosotros) // Referencia al layout
+    }
+
+}
